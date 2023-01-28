e7a0284355adc33a7a14602bb5b2ba4d6d3c8b42
Vitaliy <klaot@yandex.ru>
2023-01-28 07:06:36 +0300

    деплой сервера

---
+++ index.js
@@ -0,0 +1,40 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const {Server} = require('socket.io');
+const cors = require('cors');
+app.use(cors());
+app.options('*', cors())
+let TIMERTIME = 120;
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+        origin: "*",
+        methods: ['GET', 'POST'],
+      }
+});
+
+io.on("connection", (socket) => {
+  let userJoined = {id: socket.id, price: 5000, date: 15, assurance: 12, percent: 100, improvement: "-", timer: false, name: `Вы: ${socket.id}`}
+  socket.emit('join',  userJoined)
+  socket.emit('timer', TIMERTIME);
+})
+
+setInterval(StartTimer, 1000)
+  function StartTimer() {
+    if(TIMERTIME >= 1) {
+      TIMERTIME--
+      io.emit('timer', TIMERTIME); 
+    } else {
+      TIMERTIME = 120
+      io.emit('timer', TIMERTIME); 
+    }   
+  } 
+
+  
+server.listen(3001, ()=> {
+  console.log('start server');
+})
